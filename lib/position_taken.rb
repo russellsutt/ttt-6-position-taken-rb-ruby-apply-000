9c3b3779fb506eebe0d69d9ec499c72252bb5d9c
Russell Sutter <russell.sutter@gmail.com>
2020-06-19 00:01:21 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,9 +1,5 @@
-board = ["", "", "", "", "", "", "", "", ""]
-
 def position_taken?(board, index)
-  if (board[index] == " ") || (board[index] == "") || (board[index] == nil)
-    return false
+  if (board[index] == " ") || (board[index] == "")
   else (board[index] == "X" || (board[index] == "O")
-    return true
   end
 end
